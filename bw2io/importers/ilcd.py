c11d470dfe49d80e50bc3ea8fd1ec2cc150ee2c0
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2022-10-12 16:36:30 +0200

    trying to use migrations to change units

--- bw2io/importers/ilcd.py
+++ bw2io/importers/ilcd.py
@@ -1,7 +1,10 @@
+import functools
+
 from .base_lci import LCIImporter
 from ..extractors.ilcd import ILCDExtractor
 from ..strategies.ilcd import rename_activity_keys, get_activity_unit
 from ..strategies.generic import assign_only_product_as_production
+from ..strategies.migrations import migrate_exchanges,migrate_datasets
 
 
 class ILCDImporter(LCIImporter):
@@ -13,4 +16,6 @@ class ILCDImporter(LCIImporter):
             rename_activity_keys,
             get_activity_unit,
             assign_only_product_as_production,
+            functools.partial(migrate_datasets, migration="default-units"),
+            functools.partial(migrate_exchanges, migration="default-units"),
         ]
