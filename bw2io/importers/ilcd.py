51aee436a519d60c0a01dc119b1db5e234a4d303
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2022-11-03 21:58:07 +0100

    add strategies for default location and set production exchange

--- bw2io/importers/ilcd.py
+++ bw2io/importers/ilcd.py
@@ -2,7 +2,8 @@ import functools
 
 from .base_lci import LCIImporter
 from ..extractors.ilcd import ILCDExtractor
-from ..strategies.ilcd import rename_activity_keys, get_activity_unit,convert_to_default_units,map_to_biosphere3
+from ..strategies.ilcd import (rename_activity_keys, get_activity_unit,
+convert_to_default_units,map_to_biosphere3, set_default_location,set_production_exchange)
 from ..strategies.generic import assign_only_product_as_production
 from ..strategies.migrations import migrate_exchanges,migrate_datasets
 
@@ -15,11 +16,11 @@ class ILCDImporter(LCIImporter):
         self.strategies = [
             rename_activity_keys,
             get_activity_unit,
+            set_production_exchange,
             convert_to_default_units,
             assign_only_product_as_production,
-            # functools.partial(migrate_datasets, migration="default-units"),
-            # functools.partial(migrate_exchanges, migration="default-units"),
             map_to_biosphere3,
+            set_default_location,
 
         ]
 
@@ -29,8 +30,13 @@ class ILCDImporter(LCIImporter):
             ds['code'] = ds['uuid']
             ds['type']='process'
 
+            # technosphere flows are assigned by default the dbname and code
             for e in ds.get('exchanges'):
                 if e['type']=='Product flow':
                     e['database'] = ds['database']
-                    e['code'] = ds['code']
-                    #e['input'] = (ds['database'],ds['code'])
+                    e['code'] = ds['uuid']
+
+
+                    if e['exchanges_internal_id'] == ds['reference_to_reference_flow']:
+                        # production flow
+                        continue
