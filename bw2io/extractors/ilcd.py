c5007c403c312b2ded0607665998c990b9aa68c0
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2022-11-15 23:00:19 +0100

    start extraction of dataset parameters

--- bw2io/extractors/ilcd.py
+++ bw2io/extractors/ilcd.py
@@ -19,6 +19,8 @@ xpaths_process = {
     "location": "/processDataSet/processInformation/geography/locationOfOperationSupplyOrProduction/@location",
     "reference_to_reference_flow": "/processDataSet/processInformation/quantitativeReference/referenceToReferenceFlow/text()",
     # Xpath for values in process XML file, will return multiple values as a list
+    "parameter_name":"/processDataSet/processInformation/mathematicalRelations/variableParameter/@name",
+    "parameter_meanvalue":"/processDataSet/processInformation/mathematicalRelations/variableParameter/meanValue/text()",
     "exchanges_internal_id": "/processDataSet/exchanges/exchange/@dataSetInternalID",
     "exchanges_name": "/processDataSet/exchanges/exchange/referenceToFlowDataSet/common:shortDescription/text()",
     "exchanges_uuid": "/processDataSet/exchanges/exchange/referenceToFlowDataSet/@refObjectId",
