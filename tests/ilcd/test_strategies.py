868afeff7beef2d5fb77198dec21fbdfd8eee5ab
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2023-03-21 00:21:28 +0100

    extend tests

--- tests/ilcd/test_strategies.py
+++ tests/ilcd/test_strategies.py
@@ -6,11 +6,14 @@ def test_unit_conversion():
 
     # basic transformation to default units
     ds = {'unit':'g','exchanges':[
-    {'unit':'g','amount':1000},
-    {'unit':'Rutherford','amount':1},
+    {'unit':'g','amount':1000,'unit_reference':'kg','unit_multiplier':1e-3},
+    {'unit':'Rutherford','amount':2,'unit_reference':'kBq','unit_multiplier':1000},
     ]}
 
     db = [ds]
 
     db_converted = ilcd.convert_to_default_units(db)
-    assert math.isclose(db_converted[0]['exchanges'][0]['amount'],1.0)
+    e1,e2 = db_converted[0]['exchanges']
+
+    assert math.isclose(e1['amount'],1.0),e1
+    assert math.isclose(e2['amount'],2000),e2
