70c084b09b0d06eb1ff2996a7ce5ad6e10037e93
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2023-04-14 16:56:02 +0200

    fix unit unit test

--- tests/ilcd/test_strategies.py
+++ tests/ilcd/test_strategies.py
@@ -3,11 +3,13 @@ import math
 
 
 def test_unit_conversion():
-
-    # basic transformation to default units
+    """tests that the convert_to_default_units strategy is able to go from 
+    non-default ilcd units to default biosphere3 units.
+    """
+    # case using non-default ilcd units
     ds = {'unit':'g','exchanges':[
-    {'unit':'g','amount':1000,'unit_reference':'kg','unit_multiplier':1e-3},
-    {'unit':'Rutherford','amount':2,'unit_reference':'kBq','unit_multiplier':1000},
+    {'unit':'g','amount':1000,'unit_group':'Units of mass','unit_multiplier':1e-3},
+    {'unit':'Rutherford','amount':2,'unit_group':'Units of radioactivity','unit_multiplier':1000},
     ]}
 
     db = [ds]
@@ -16,4 +18,6 @@ def test_unit_conversion():
     e1,e2 = db_converted[0]['exchanges']
 
     assert math.isclose(e1['amount'],1.0),e1
+    assert e1['unit'] == 'kilogram',e1
     assert math.isclose(e2['amount'],2000),e2
+    assert e2['unit'] == 'kilo Becquerel'
