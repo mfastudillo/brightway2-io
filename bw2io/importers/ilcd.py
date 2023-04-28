1d5221f4eb6333b8d41357b2c6f61e3631e05de1
Miguel F. Astudillo <miguelfastudillo@gmail.com>
2023-04-28 20:35:32 +0200

    update strategy name

--- bw2io/importers/ilcd.py
+++ bw2io/importers/ilcd.py
@@ -2,7 +2,7 @@ import functools
 
 from .base_lci import LCIImporter
 from ..extractors.ilcd import ILCDExtractor
-from ..strategies.ilcd import (rename_activity_keys, get_activity_unit,
+from ..strategies.ilcd import (rename_activity_keys, set_activity_unit,
 convert_to_default_units,map_to_biosphere3, set_default_location,
 set_production_exchange,setdb_and_code,remove_clutter)
 from ..strategies.generic import assign_only_product_as_production
@@ -20,7 +20,7 @@ class ILCDImporter(LCIImporter):
             rename_activity_keys,
             set_production_exchange,
             convert_to_default_units,
-            get_activity_unit,
+            set_activity_unit,
             assign_only_product_as_production,
             map_to_biosphere3,
             set_default_location,
